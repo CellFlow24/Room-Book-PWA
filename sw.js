3a3b1358bda5ec5b0d097c917b2b051953e7b923
CellFlow24 <cellflow24@gmail.com>
2026-07-21 10:54:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,45 @@
+const CACHE_NAME = 'roombook-v1'; 
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/logo.png' // Added your logo so it works offline too!
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('roombook-cache').then((cache) => {
-    return cache.addAll(['/', '/index.html', '/style.css', '/app.js', '/manifest.json']);
-  }));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting(); 
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // If the cache name doesn't match our current version, delete it
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    })
+  );
+  // Ensure the service worker takes control of the page immediately
+  self.clients.claim(); 
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
